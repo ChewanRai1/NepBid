a62385eed22b4aabcc3b84eeedeba4136deda26c
ChewanRai1 <220154@softwarica.edu.np>
2025-01-24 22:08:19 +0545

    modified Auctions.jsx

--- frontendnb/src/pages/Auctions.jsx
+++ frontendnb/src/pages/Auctions.jsx
@@ -10,7 +10,7 @@ const Auctions = () => {
       {loading ? (
         <Spinner />
       ) : (
-        <article className="w-full ml-0 m-0 h-fit px-5 pt-20 lg:pl-[320px] flex flex-col">
+        <article className="w-full h-fit px-5 pt-20  flex flex-col">
           <section className="my-8">
             <h1
               className={`text-[#d6482b] text-2xl font-bold mb-2 min-[480px]:text-4xl md:text-6xl xl:text-7xl 2xl:text-8xl`}
